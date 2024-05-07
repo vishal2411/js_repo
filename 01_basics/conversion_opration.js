ec4be934ca3ed9df54185383879e791d984a4f74
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2024-05-07 18:43:15 +0000

    conversion of datatype

---
+++ 01_basics/conversion_opration.js
@@ -0,0 +1,28 @@
+let score = "33kkk"
+
+console.log(typeof score)
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//"33" = 33
+//"33abc" = NAN
+//true => 1; false => 0
+
+let isloggedin = 0
+
+let booleanIsLoggedin = Boolean(isloggedin);
+console.log(booleanIsLoggedin);
+
+//1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
