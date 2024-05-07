985001f3556e9849758d7105c5bba68edae96b47
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2024-05-07 18:52:16 +0000

    changes

--- 01_basics/conversion_opration.js
+++ 01_basics/conversion_opration.js
@@ -1,13 +1,13 @@
 let score = "33kkk"
 
-console.log(typeof score)
+//console.log(typeof score)
 
-console.log(typeof (score));
+//console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 //"33" = 33
 //"33abc" = NAN
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isloggedin = 0
 
 let booleanIsLoggedin = Boolean(isloggedin);
-console.log(booleanIsLoggedin);
+//console.log(booleanIsLoggedin);
 
 //1 => true; 0=> false
 // "" => false
@@ -24,5 +24,17 @@ console.log(booleanIsLoggedin);
 
 let someNumber = 22
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//********************* Oprations********************* */
+
+let value = 3
+let negvalue = -value;
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = "vishal"
+
+let str3 = str1 + str2;
+console.log(str3)
