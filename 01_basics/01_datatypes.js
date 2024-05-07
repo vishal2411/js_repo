8817dd6094276fd77d0ccb9833c4d9a41c8a3f49
vishal2411 <71117240+vishal2411@users.noreply.github.com>
2024-05-07 18:24:30 +0000

    datatypes file

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all code as newer version
+
+//alert(3 + 3);  // we are using node js not browser
+
+// tc39 //mdn
+
+let name = "Bigboss";
+let age = 18
+let isloggedin = true;
+
+//primitive type
+//number => 2 to power 53
+// bigint
+// string => ""
+// null => standalone value
+// boolean => true/false
+// undefined
+// symbole = unique
+
+// non-primitive type
+
+//object
+
+console.log(typeof null);
